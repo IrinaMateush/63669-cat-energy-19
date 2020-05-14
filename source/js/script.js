79b5993d746cf7808048c319b18cd1c2f423fd2b
Irina Mateush <ismateush@yandex-team.ru>
2020-05-14 21:41:23 +0300

    фиксы к защите

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,19 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var menuList = document.querySelector('.site-list');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
+  if (menuList.classList.contains('site-list--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    menuList.classList.remove('site-list--closed');
+    menuList.classList.add('site-list--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+    menuList.classList.remove('site-list--opened');
+    menuList.classList.add('site-list--closed');
   }
 });
